f6b57a6da63d2fd3ec4dc949d23791d7594fe26a
cerkeira <ricardo.cerqueira@ua.pt>
2023-09-21 12:14:37 +0100

    nova pagina

---
+++ src/app/pages/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from "next/link";
+
+
+export default function Nova() {
+
+    
+  return (
+    <div className="nova">
+Crazy Test
+<Image></Image>
+
+    </div>
+    )
+}
