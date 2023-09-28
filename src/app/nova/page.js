030f48008cc7e3d5760e05053d734cb1d2287093
cerkeira <ricardo.cerqueira@ua.pt>
2023-09-28 10:34:24 +0100

    prettier

--- src/app/nova/page.js
+++ src/app/nova/page.js
@@ -1,19 +1,18 @@
-import Image from 'next/image'
-import styles from '../page.module.css'
-import Link from 'next/link'
-
+import Image from "next/image";
+import styles from "../page.module.css";
+import Link from "next/link";
 
 export default function Nova() {
-
-    
   return (
     <div className="nova">
-<Link href="/">
-<Image src="/next.svg"          className={styles.vercelLogo}
- width={100} height={100}></Image>
- </Link>
-
-
+      <Link href="/">
+        <Image
+          src="/next.svg"
+          className={styles.vercelLogo}
+          width={100}
+          height={100}
+        ></Image>
+      </Link>
     </div>
-    )
+  );
 }
