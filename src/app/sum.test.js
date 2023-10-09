37cd861bd408c68accd5f595445927ba51e36b6c
cerkeira <ricardo.cerqueira@ua.pt>
2023-10-09 18:13:32 +0100

    new src

--- src/app/sum.test.js
+++ src/app/sum.test.js
@@ -1,15 +1,8 @@
 
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Home from './src/app/page'
+import Home from './page'
 import React from 'react'
-
-const sum = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
 
 test('Sou Lindo?', async () => {
 render(<Home></Home>);
