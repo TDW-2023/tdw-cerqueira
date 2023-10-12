4158cf97e896348e157277a302920c44dc216f5f
cerkeira <ricardo.cerqueira@ua.pt>
2023-10-12 09:18:37 +0100

    fix prettier

--- src/app/Nova.test.js
+++ src/app/Nova.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import Nova from './page';
 
 test('snapshot para  o Componente de teste', () => {
-  const component = renderer.create(<Nova/>);
+  const component = renderer.create(<Nova />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
